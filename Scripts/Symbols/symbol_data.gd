d027bbeb100ea4b27d4734e0a801a180c7666250
Andree Burlamaqui <umdreeb@gmail.com>
2026-02-06 19:22:12 -0300

    Created symbol data and resources

---
+++ Scripts/Symbols/symbol_data.gd
@@ -0,0 +1,6 @@
+class_name SymbolData extends Resource
+
+@export var id: String = "fire" # Debugging mostly
+@export var display_name: String = "Fire"
+@export var icon: Texture2D
+@export var color: Color = Color.WHITE
