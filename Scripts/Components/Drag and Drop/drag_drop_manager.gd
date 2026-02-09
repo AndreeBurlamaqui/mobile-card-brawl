456aee5ef11210846ec8bc830543d70090af4b27
Andree Burlamaqui <umdreeb@gmail.com>
2026-02-09 14:42:23 -0300

    Added shadow on drag to "lift"

--- Scripts/Components/Drag and Drop/drag_drop_manager.gd
+++ Scripts/Components/Drag and Drop/drag_drop_manager.gd
@@ -3,15 +3,15 @@ extends Node
 signal drag_started(draggable: DraggableComponent)
 signal drag_ended(draggable: DraggableComponent, success: bool)
 
-@export var debug_mode: bool = true
-
 # -- Configuration --
 var drag_drop_canvas: CanvasLayer
 var drag_speed: float = 35
+var shadow_offset := Vector2(0, 15)
 
 # -- State --
 var current_drag: DraggableComponent = null
 var current_visual: Control = null
+var current_shadow: Control = null
 var drag_offset: Vector2 = Vector2.ZERO
 
 var _all_droppables: Array[DroppableComponent] = []
@@ -42,6 +42,9 @@ func _process(delta: float) -> void:
 		var mouse_pos = get_viewport().get_mouse_position()
 		var lerpTime = (1 - cos(PI * (delta * drag_speed))) / 2 #ease in-out
 		current_visual.global_position = current_visual.global_position.lerp(mouse_pos - drag_offset, lerpTime)
+		
+		if current_shadow:
+			current_shadow.global_position = current_visual.global_position + shadow_offset
 	
 	# Search for droppable zones
 	if current_drag:
@@ -77,6 +80,7 @@ func start_drag(draggable: DraggableComponent) -> void:
 	# Store original visual info
 	var original_size = current_drag.visual.size
 	var original_pos = current_drag.visual.global_position
+	var final_scale = current_drag.visual.scale * 1.35
 	
 	# Duplicate the Visual
 	current_visual = current_drag.visual.duplicate(8) # 8 use instantiate()
@@ -84,14 +88,27 @@ func start_drag(draggable: DraggableComponent) -> void:
 	current_visual.set_anchors_preset(Control.PRESET_TOP_LEFT) # Neutral layout
 	current_visual.size = original_size
 	drag_offset = original_size * current_visual.pivot_offset_ratio + current_visual.pivot_offset
-	var pickTween = create_tween().set_parallel(true)
+	
+	# Create the shadow for "lifting effect"
+	current_shadow = current_visual.duplicate(8)
+	current_shadow.modulate = Color(0, 0, 0, 0.4)
+	current_shadow.rotation = 0
+	current_shadow.scale = final_scale
+	
+	# Prepare tweening
+	var pickTween = create_tween().set_parallel(true).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 	pickTween.tween_property(current_visual, "rotation", 0, 0.15)
-	pickTween.tween_property(current_visual, "scale", current_drag.visual.scale * 1.35, 0.15)
-	pickTween.play()
+	pickTween.tween_property(current_visual, "scale", final_scale, 0.2)
 	
+	# Finish setup
+	shadow_parallax.add_child(current_shadow)
 	drag_drop_canvas.add_child(current_visual)
-	current_visual.global_position = original_pos
 	
+	# Start position for tweening
+	current_visual.global_position = original_pos
+	current_shadow.global_position = original_pos
+	
+	pickTween.play()
 	draggable.on_drag_started()
 	drag_started.emit(draggable)
 
@@ -111,12 +128,13 @@ func end_drag(success: bool = false) -> void:
 		# Quick tween it back to og pos
 		var lastVisual = current_visual
 		var initial_position = drag_dropped.target.global_position
-		var move_back_tween = create_tween()
-		move_back_tween.tween_property(lastVisual, "global_position", initial_position, 0.15)
+		var move_back_tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BACK)
+		move_back_tween.tween_property(lastVisual, "global_position", initial_position, 0.25)
 		await move_back_tween.finished
 	
 	drag_dropped.on_drag_ended()
 	current_visual.queue_free()
+	current_shadow.queue_free()
 	drag_ended.emit(drag_dropped, success)
 	current_drag = null
 	_last_hovered_droppable = null
