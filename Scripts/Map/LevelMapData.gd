2764f198ba32ba13db071d2dbdbd59abacd2b83b
Andree Burlamaqui <umdreeb@gmail.com>
2026-02-10 11:09:51 -0300

    Creating prototype of level data

---
+++ Scripts/Map/LevelMapData.gd
@@ -0,0 +1,41 @@
+class_name LevelMapData extends Resource
+
+@export_group("Grid Structure")
+@export var grid_height: int = 15
+@export_range(1, 5) var grid_width: int = 5
+## Chance that a node will connect to 2 nodes in the next row instead of 1
+@export_range(0.0, 100.0) var _branch_chance: float = 30
+## Chance that a path simply merges into a neighbor lane instead of moving up
+@export_range(0.0, 100.0) var _deadend_merge_chance: float = 1
+
+# TEMP: Later this will be modular for each data
+@export_group("Room Weights")
+@export var weight_mob: int = 50
+@export var weight_camp: int = 15
+@export var weight_chest: int = 10
+@export var weight_event: int = 25
+
+@export_group("Enemy Specifics")
+## If a room is a MOB, chance it becomes an ELITE
+@export_range(0.0, 100.0) var _elite_chance: float = 15
+
+# Helper to get a random room type based on weights
+func pick_weighted_room_type() -> String:
+	var total_weight = weight_mob + weight_camp + weight_chest + weight_event
+	var roll = randi() % total_weight
+	
+	if roll < weight_mob: return "MOB"
+	roll -= weight_mob
+	
+	if roll < weight_camp: return "CAMP"
+	roll -= weight_camp
+	
+	if roll < weight_chest: return "CHEST"
+	
+	return "EVENT"
+
+func is_deadend() -> bool:
+	return randf() < (_deadend_merge_chance / 100.0)
+
+func is_elite() -> bool:
+	return randf() < (_elite_chance / 100.0)
--- Scripts/Map/MapGenerator.gd
+++ Scripts/Map/MapGenerator.gd
@@ -1,16 +1,15 @@
 extends Control
 
 @export_category("Map Settings")
-@export var grid_height: int = 15 # TODO: Change to level data
-@export var grid_width: int = 5 # TODO: Change to level data
+@export var data: LevelMapData
 ## To make room positioning uneven, without respecting the grid too much
-@export var position_jitter: float = 25.0
+@export var _position_jitter: float = 25.0
 
 @export_category("References")
-@export var scroller: ScrollContainer
-@export var line_layer: Control
-@export var rooms_layer: Control
-@export var grid_slots: GridContainer
+@export var _scroller: ScrollContainer
+@export var _line_layer: Control
+@export var _rooms_layer: Control
+@export var _grid_slots: GridContainer
 @export var _encounter_node: PackedScene
 
 # Data Class
@@ -25,8 +24,13 @@ class MapNodeData:
 var _grid_data: Array = [] # 2D array [row][col]
 
 func _ready() -> void:
+	if not data:
+		push_error("MapGenerator: No Level Data assigned!")
+		return
+	
 	# Create the level map
-	_generate_data()
+	_generate_grid_data()
+	_assign_room_types()
 	
 	# Create all the slots to be the base position of the room visual
 	_create_slots()
@@ -39,62 +43,78 @@ func _ready() -> void:
 	_spawn_visuals()
 	
 	# Draw connecting Lines
-	line_layer.draw.connect(_on_line_layer_draw)
-	line_layer.queue_redraw()
+	_line_layer.draw.connect(_on_line_layer_draw)
+	_line_layer.queue_redraw()
 	
 	# Scroll to (Start)
 	# call_deferred to ensure the scrollbar max_value is updated
 	call_deferred("_scroll_to_bottom")
 
 func _scroll_to_bottom() -> void:
-	scroller.scroll_vertical = scroller.get_v_scroll_bar().max_value
+	_scroller.scroll_vertical = _scroller.get_v_scroll_bar().max_value
 
-# ==============================================================================
-# DATA GENERATION
-# ==============================================================================
-func _generate_data() -> void:
+func _generate_grid_data() -> void:
 	_grid_data.clear()
 	seed(Time.get_unix_time_from_system()) # TODO: Add option for custom seed
 	
+	var height = data.grid_height
+	var width = data.grid_width
+	
 	# Init Empty Grid
-	for r in range(grid_height):
+	for r in range(height):
 		var row = []
-		for c in range(grid_width): row.append(null)
+		for c in range(width): row.append(null)
 		_grid_data.append(row)
 
 	# Pick a random column for the single start node
-	var start_col = randi() % grid_width
+	var start_col = randi() % width
 	var start_node = MapNodeData.new(Vector2i(0, start_col))
 	start_node.type = "START"
 	_grid_data[0][start_col] = start_node
 
 	# Create branches
-	for r in range(grid_height - 2):
-		_connect_row_upwards(r)
+	for r in range(height - 2):
+		_connect_rooms(r)
 
 	# Final Boss
-	var boss_row = grid_height - 1
-	var boss_col = floor(grid_width / 2.0)
+	var boss_row = height - 1
+	var boss_col = floor(width / 2.0)
 	var boss = MapNodeData.new(Vector2i(boss_row, boss_col))
 	boss.type = "BOSS"
 	_grid_data[boss_row][boss_col] = boss
 	
 	# Connect Pre-Boss row to Boss
-	for c in range(grid_width):
+	for c in range(width):
 		var node = _grid_data[boss_row - 1][c]
 		if node: node.outgoing.append(boss.grid_pos)
 
-func _connect_row_upwards(row_idx: int) -> void:
+func _connect_rooms(row_idx: int) -> void:
+	# It'll either connect to the next (upwards) or the neighbours (sides)
 	var next_row = row_idx + 1
-	for c in range(grid_width):
-		var node = _grid_data[row_idx][c]
+	var width = data.grid_width
+	for column in range(width):
+		var node = _grid_data[row_idx][column]
 		if not node: continue
+		
+		# Check deadends first if it's not the START
+		if row_idx > 0 and data.is_deadend():
+			var side_targets = []
+			if column > 0 and _grid_data[row_idx][column-1] != null:
+				side_targets.append(column - 1)
+			if column < width - 1 and _grid_data[row_idx][column+1] != null:
+				side_targets.append(column + 1)
+			
+			if side_targets.size() > 0:
+				var target_c = side_targets.pick_random()
+				# Connect to neighbor in SAME row
+				node.outgoing.append(Vector2i(row_idx, target_c))
+				continue # Valid deadend, no upward path
 		
 		# Define valid moves (Left, Center, Right)
 		var targets = []
-		if c > 0: targets.append(c - 1)
-		targets.append(c)
-		if c < grid_width - 1: targets.append(c + 1)
+		if column > 0: targets.append(column - 1)
+		targets.append(column)
+		if column < width - 1: targets.append(column + 1)
 		targets.shuffle()
 		
 		# TODO: Make this be a data from the level
@@ -112,15 +132,36 @@ func _connect_row_upwards(row_idx: int) -> void:
 				node.outgoing.append(t_pos)
 			
 			if _grid_data[next_row][t_col] == null:
+				# Create node if it doesn't exist yet
 				_grid_data[next_row][t_col] = MapNodeData.new(t_pos)
+
+func _assign_room_types() -> void:
+	var height = data.grid_height
+	var width = data.grid_width
+	
+	# Skip Start (Row 0) and Boss (Row height-1)
+	for row in range(1, height - 1):
+		for column in range(width):
+			var node = _grid_data[row][column]
+			if not node: continue
+			
+			# Set room type
+			var base_type = data.pick_weighted_room_type()
+			
+			# In case it's an enemy, check tier
+			if base_type == "MOB":
+				if data.is_elite():
+					node.type = "ELITE"
+				else:
+					node.type = "MOB"
+			else:
+				node.type = base_type
 
 func _create_slots() -> void:
 	# Fill GridContainer with spacers to define the "perfect grid" positions
-	# Get node size
-	
 	# Setup grid
-	for child in grid_slots.get_children(): child.queue_free() # Remove placeholders
-	grid_slots.columns = grid_width # Ensure it'll be like data
+	for child in _grid_slots.get_children(): child.queue_free() # Remove placeholders
+	_grid_slots.columns = data.grid_width # Ensure it'll be like data
 	
 	# Instantiate and insta delete encounter node so we get the slot size
 	var temp_instance = _encounter_node.instantiate() as Control
@@ -129,9 +170,11 @@ func _create_slots() -> void:
 	
 	# GridContainer fills Top-Left to Bottom-Right
 	# [0]=Bottom. We iterate REVERSE row index.
-	for r in range(grid_height - 1, -1, -1):
-		for c in range(grid_width):
-			var spacer = Panel.new()
+	var height = data.grid_height
+	var width = data.grid_width
+	for row in range(height - 1, -1, -1):
+		for column in range(width):
+			var spacer = Panel.new() # TEMP to see what's happening BTS
 			
 			# Setup spacer
 			spacer.custom_minimum_size = spacer_size
@@ -139,79 +182,81 @@ func _create_slots() -> void:
 			spacer.size_flags_vertical = Control.SIZE_EXPAND_FILL
 			
 			# Debug purposes
-			spacer.name = "Anchor_%d_%d" % [r, c]
+			spacer.name = "Anchor_%d_%d" % [row, column]
 			spacer.modulate.a = 0.15
 			
-			grid_slots.add_child(spacer)
+			_grid_slots.add_child(spacer)
 
 func _spawn_visuals() -> void:
-	for r in range(grid_height):
-		for c in range(grid_width):
-			var data = _grid_data[r][c]
+	var height = data.grid_height
+	var width = data.grid_width
+	
+	for row in range(height):
+		for column in range(width):
+			var data = _grid_data[row][column]
 			if not data: continue
 			
 			# Get the slot
-			var anchor_idx = (grid_height - 1 - r) * grid_width + c
-			var anchor = grid_slots.get_child(anchor_idx)
+			var anchor_idx = (height - 1 - row) * width + column
+			var anchor = _grid_slots.get_child(anchor_idx)
 			
 			# Add jitter offset to look less like a grid
-			var offset = Vector2.ZERO
-			if data.type != "START" and data.type != "BOSS":
-				# Start with full range
-				var min_x = -position_jitter
-				var max_x = position_jitter
-				
-				# Ensure it'll not go outside viewport
-				if c == 0:
-					# First column: Never go left of center (0)
-					min_x = 0.0
-				if c == grid_width - 1:
-					# Last column: Never go right of center (0)
-					max_x = 0.0
-				
-				# Ensure it'll not overlap with other rooms
-				if c > 0 and _grid_data[r][c-1] != null:
-					# If neighbor to the left, try to stay to the right (+5)
-					min_x = max(min_x, 5.0)
-				if c < grid_width - 1 and _grid_data[r][c+1] != null:
-					# If neighbor to the right, try to stay to the left (-5)
-					max_x = min(max_x, -5.0)
-				
-				var jitter_x = 0.0
-				
-				if min_x < max_x:
-					# If we are squeezed (e.g., Wall says >0 but Neighbor says <-5),
-					# we default to 0 (Center) to be safe.
-					jitter_x = randf_range(min_x, max_x)
-				else:
-					# The constraints conflict or are tight (e.g., between wall and neighbor)
-					# Just pick the average or 0
-					jitter_x = lerp(min_x, max_x, 0.5)
-				
-				var jitter_y = randf_range(-position_jitter, position_jitter)
-				offset = Vector2(jitter_x, jitter_y)
+			var offset = _get_position_jitter(row, column, width)
 			
 			# Finish setup
 			var center_pos = anchor.position + (anchor.size / 2.0)
 			var vis = _encounter_node.instantiate()
-			rooms_layer.add_child(vis)
+			_rooms_layer.add_child(vis)
 			
 			vis.position = center_pos + offset - (vis.size / 2.0)
 			
 			data.visual_instance = vis
 			_setup_visual_appearance(vis, data)
 
+func _get_position_jitter(row: int, column: int, width: int) -> Vector2:
+	# Don't jitter special nodes
+	var type = _grid_data[row][column].type
+	if type == "START" or type == "BOSS": return Vector2.ZERO
+	
+	var max_x = _position_jitter
+	var min_x = _position_jitter * -1
+	
+	# Wall Constraints
+	if column == 0: min_x = 0.0
+	if column == width - 1: max_x = 0.0
+	
+	# Neighbor Constraints
+	if column > 0 and _grid_data[row][column - 1] != null:
+		min_x = max(min_x, 5.0)
+	if column < width - 1 and _grid_data[row][column + 1] != null:
+		max_x = min(max_x, -5.0)
+	
+	var j_x = 0.0
+	if min_x < max_x: j_x = randf_range(min_x, max_x)
+	else: j_x = lerp(min_x, max_x, 0.5)
+	
+	return Vector2(j_x, randf_range(_position_jitter * -1, _position_jitter))
+
 func _setup_visual_appearance(vis, data):
 	# TEMP: We'll move it to the encounter visual later
 	var lbl = vis.get_node("Panel/Label")
 	lbl.text = data.type
-	if data.type == "BOSS": vis.modulate = Color.RED
-	elif data.type == "START": vis.modulate = Color.GREEN
+	match data.type:
+		"START": vis.modulate = Color.GREEN
+		"BOSS": vis.modulate = Color.RED
+		"ELITE": vis.modulate = Color.DARK_MAGENTA
+		"CAMP": vis.modulate = Color.FOREST_GREEN
+		"CHEST": vis.modulate = Color.GOLD
+		"EVENT": vis.modulate = Color.VIOLET
+		"MOB": vis.modulate = Color.WHITE
 
 func _on_line_layer_draw() -> void:
-	for r in range(grid_height):
-		for c in range(grid_width):
-			var start_node = _grid_data[r][c]
+	var height = data.grid_height
+	var width = data.grid_width
+	
+	for row in range(height):
+		for column in range(width):
+			var start_node = _grid_data[row][column]
 			if not start_node or not start_node.visual_instance: continue
 			
 			var start_pos = start_node.visual_instance.position + (start_node.visual_instance.size / 2.0)
@@ -222,4 +267,4 @@ func _on_line_layer_draw() -> void:
 					var end_pos = end_node.visual_instance.position + (end_node.visual_instance.size / 2.0)
 					
 					# TEMP: Straight line. Change to texture later
-					line_layer.draw_line(start_pos, end_pos, Color.WHITE, 4.0, true)
+					_line_layer.draw_line(start_pos, end_pos, Color.WHITE, 4.0, true)
